02e24f0be794efd2a805109423ed5cef21ec48f2
Ilkka Oksanen <iao@iki.fi>
2009-11-03 20:19:31 +0000

    New login framework separation.

--- source/class/client/Application.js
+++ source/class/client/Application.js
@@ -24,45 +24,34 @@ qx.Class.define("client.Application",
 	    if (ralph_port == 80)
 	    {
 		ralph_domain = "http://a167.myrootshell.com";
-
-		//production
-		__rpc = new qx.io.remote.Rpc(
-		    "http://a167.myrootshell.com:8080/lisa/jsonrpc.pl",
-		    "lisa.main" );
 	    }
 	    else
 	    {
 		ralph_domain = "http://evergreen_dev.portaali.org:9999";
-
-		__rpc = new qx.io.remote.Rpc(
-		    "http://evergreen_dev.portaali.org/lisa/jsonrpc.pl",
-		    "lisa.main" );
 	    }
 
 	    infoDialog = new client.InfoDialog();
+	
+	    var query = window.location.search;
 
-	    __rpc.setCrossDomain(true);
+	    global_id = this.gup('id');
+	    global_sec = this.gup('token');
 
-	    var cookie = qx.bom.Cookie.get("ProjectEvergreen");
-
-	    loginForm = new client.Login(__rpc, this);
-
-	    if(cookie !== null)
-	    {
-		__rpc.callAsync(loginForm.result, "login_cookie", cookie);
-	    }
-
-	    registrationForm = new client.Registration(__rpc);
-
-	    loginForm.show(this.getRoot());
-
-	},
-
-	loginDone : function()
-	{
 	    this.getRoot().removeAll();
 	    mainView = new client.MainScreen();
 	    mainView.show(this.getRoot());
+	},
+
+	gup : function( name )
+	{
+	    name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+	    var regexS = "[\\?&]"+name+"=([^&#]*)";
+	    var regex = new RegExp( regexS );
+	    var results = regex.exec( window.location.href );
+	    if( results == null )
+		return "";
+	    else
+		return results[1];
 	}
     }
 });
