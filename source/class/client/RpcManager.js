227f83c37e1925fdbe03d3824a1fd206da317951
Ilkka Oksanen <iao@iki.fi>
2010-04-02 20:43:59 +0000

    Added RpcManager

---
+++ source/class/client/RpcManager.js
@@ -0,0 +1,113 @@
+/* ************************************************************************
+
+#asset(projectx/*)
+#require(qx.util.StringSplit)
+
+************************************************************************ */
+
+qx.Class.define("client.RpcManager",
+{
+    extend : qx.core.Object,
+
+    construct : function(desktop, system, name)
+    {
+	this.base(arguments);
+	
+	// write "socket"
+	this.__srpc = new qx.io.remote.Rpc("/ralph", "ralph");
+	this.__srpc.setTimeout(10000);
+    },
+
+    members :
+    {
+	id : 0,
+	sec : 0,
+	cookie : 0,
+	infoDialog : 0,
+
+	__srpc : 0,
+	
+	call : function(command, parameters, context)
+	{
+	    this.__srpc.callAsync(
+		qx.lang.Function.bind(this.__sendresult, context),
+		command, this.id + " " + this.sec + " " + this.cookie + " " +
+		    parameters);
+	},
+
+	read : function(command, parameters, context, callback)
+	{
+	    this.__srpc.callAsync(
+		qx.lang.Function.bind(callback, context),
+		command, this.id + " " + this.sec + " " + this.cookie + " " +
+		    parameters);
+	},
+	
+	__sendresult : function(result, exc)
+	{
+	    if (exc == null) 
+	    {
+		//TODO: BIG HACK -> fix the protocol!!!
+		if (result.charAt(0) == "1")
+		{
+		    result = result.substr(2);
+		}
+
+		var pos = result.search(/ /);
+		var command = result.slice(0, pos);
+		
+		if (command == "OPERLIST")
+		{
+		    result = result.slice(pos+1);
+		    var opers = result.split("<<>>"); 
+		    
+		    this.configListOper.removeAll();
+		    
+		    for (var i=0; i < opers.length; i++)
+		    {
+			var tmp = opers[i].split("<>");
+			var tmpList = new qx.ui.form.ListItem(tmp[1]);
+			tmpList.userid = tmp[0];
+			this.configListOper.add(tmpList);
+		    }
+		}
+		else if (command == "BANLIST")
+		{
+		    result = result.slice(pos+1);
+		    var bans = result.split("<<>>"); 
+		    
+		    this.configListBan.removeAll();
+
+		    for (var i=0; i < bans.length; i++)
+		    {
+			var tmp = bans[i].split("<>");
+			var tmpList = new qx.ui.form.ListItem(tmp[0]);
+			tmpList.banid = tmp[1];
+			this.configListBan.add(tmpList);
+		    }
+		}
+		else if (command == "DIE")
+		{
+		    this.infoDialog.showInfoWin(
+			"Session terminated. <p>Press OK to restart.",
+			"OK", function () {
+			    qx.bom.Cookie.del("ProjectEvergreen");
+			    window.location.reload(true);
+			});
+		}
+		else if (command == "INFO")
+		{
+                    var param = result.slice(pos+1);
+		    this.infoDialog.showInfoWin(param, "OK");
+		}
+	    }
+	    else 
+	    {
+		this.infoDialog.showInfoWin("Lost connection to server.<p>Trying to recover...");
+
+		qx.event.Timer.once(
+		    function(e) { window.location.reload(true); }, this, 2000);
+	    }
+	}
+    }
+});
