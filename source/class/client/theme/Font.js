33cc9bf35177e741b7a8c1e89efcb75ea0bfb6e3
Ilkka Oksanen <iao@iki.fi>
2010-04-14 10:38:40 +0000

    Font test 2

--- source/class/client/theme/Font.js
+++ source/class/client/theme/Font.js
@@ -21,7 +21,7 @@ qx.Theme.define("client.theme.Font",
 	    family : qx.bom.client.Platform.MAC ? [ "Lucida Grande" ] :
 		(qx.bom.client.System.WINVISTA || qx.bom.client.System.WIN7) ?
 		[ "Segoe UI", "Candara" ] :
-		[ "Liberation Sans", "Tahoma", "Arial", "sans-serif" ]
+		[ "Arial", "Liberation Sans", "Tahoma",  "sans-serif" ]
 	}
     }
 });
