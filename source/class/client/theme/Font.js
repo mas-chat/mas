c7162963a1efebd7009856cae4a43750e80e7371
Ilkka Oksanen <iao@iki.fi>
2011-04-09 16:47:13 +0000

    Qooxdoo 1.3 -> 1.4

--- source/class/client/theme/Font.js
+++ source/class/client/theme/Font.js
@@ -16,10 +16,10 @@ qx.Theme.define("client.theme.Font",
     {
 	"defaultlarge" :
 	{
-	    size : (qx.bom.client.System.WINVISTA || qx.bom.client.System.WIN7) ? 13 : 13,
+	    size : ((qx.core.Environment.get("os.name") === "win" && qx.core.Environment.get("os.version") === "vista") || (qx.core.Environment.get("os.name") === "win" && qx.core.Environment.get("os.version") === "7")) ? 13 : 13,
 	    lineHeight : 1.4,
-	    family : qx.bom.client.Platform.MAC ? [ "Lucida Grande" ] :
-		(qx.bom.client.System.WINVISTA || qx.bom.client.System.WIN7) ?
+	    family : (qx.core.Environment.get("os.name") === "osx") ? [ "Lucida Grande" ] :
+		((qx.core.Environment.get("os.name") === "win" && qx.core.Environment.get("os.version") === "vista") || (qx.core.Environment.get("os.name") === "win" && qx.core.Environment.get("os.version") === "7")) ?
 		[ "Segoe UI", "Candara" ] :
 		[ "Arial", "Liberation Sans", "Tahoma",  "sans-serif" ]
 	}
