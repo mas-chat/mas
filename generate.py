d809687c4f727dd54c33a970e4817d0733550107
Ilkka Oksanen <iao@iki.fi>
2013-07-28 23:35:05 +0000

    Update to Qooxdoo 3.0

--- generate.py
+++ generate.py
@@ -25,7 +25,7 @@
 import sys, os, re, subprocess
 
 CMD_PYTHON = '/usr/bin/python2.7'
-QOOXDOO_PATH = '/opt/qooxdoo-2.1-sdk'
+QOOXDOO_PATH = '/opt/qooxdoo'
 
 def getQxPath():
     path = QOOXDOO_PATH
