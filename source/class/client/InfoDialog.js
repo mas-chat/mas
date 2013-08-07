e6cae93a8e436bb0ba34affabab6245a2270d981
Ilkka Oksanen <iao@iki.fi>
2013-08-06 20:44:30 -0700

    Polished InfoDialog.js

--- source/class/client/InfoDialog.js
+++ source/class/client/InfoDialog.js
@@ -14,7 +14,9 @@
 //   governing permissions and limitations under the License.
 //
 
-qx.Class.define("client.InfoDialog",
+'use strict';
+
+qx.Class.define('client.InfoDialog',
 {
     extend : qx.core.Object,
 
@@ -23,9 +25,9 @@ qx.Class.define("client.InfoDialog",
         this.base(arguments);
 
         this.rpc = srpc;
-        this.__window = new qx.ui.window.Window("X");
+        this.__window = new qx.ui.window.Window();
         this.__window.setLayout(new qx.ui.layout.VBox(10));
-        this.__window.set({contentPadding: [13,13,13,13]});
+        this.__window.set({ contentPadding: [13, 13, 13, 13] });
 
         this.__window.setModal(true);
         this.__window.setShowClose(false);
@@ -33,35 +35,33 @@ qx.Class.define("client.InfoDialog",
         this.__window.setShowMaximize(false);
         this.__window.moveTo(400, 300);
 
-        this.__message = new qx.ui.basic.Atom("", "");
+        this.__message = new qx.ui.basic.Atom();
         this.__message.setRich(true);
 
-        this.__message2 = new qx.ui.basic.Atom("", "");
+        this.__message2 = new qx.ui.basic.Atom();
         this.__message2.setRich(true);
 
-        this.__message3 = new qx.ui.basic.Atom("", "");
+        this.__message3 = new qx.ui.basic.Atom();
         this.__message3.setRich(true);
 
-        this.__message4 = new qx.ui.form.CheckBox("Don't show again");
+        this.__message4 = new qx.ui.form.CheckBox('Don\'t show again');
 
-        this.__box = new qx.ui.container.Composite;
-        this.__box.setLayout(new qx.ui.layout.HBox(10, "left"));
+        this.__box = new qx.ui.container.Composite();
+        this.__box.setLayout(new qx.ui.layout.HBox(10, 'left'));
         this.__spacer = new qx.ui.core.Spacer(30);
-
         this.__combo = new qx.ui.form.ComboBox();
 
-        this.__box2 = new qx.ui.container.Composite;
-        this.__box2.setLayout(new qx.ui.layout.HBox(10, "left"));
+        this.__box2 = new qx.ui.container.Composite();
+        this.__box2.setLayout(new qx.ui.layout.HBox(10, 'left'));
 
-        this.__yesbutton = new qx.ui.form.Button("Yes");
+        this.__yesbutton = new qx.ui.form.Button('Yes');
+        this.__nobutton = new qx.ui.form.Button('No');
 
-        this.__nobutton = new qx.ui.form.Button("No");
-
-        this.__input = new qx.ui.form.TextField("").set({
+        this.__input = new qx.ui.form.TextField().set({
             maxLength: 25
         });
 
-        this.__input2 = new qx.ui.form.TextField("").set({
+        this.__input2 = new qx.ui.form.TextField().set({
             maxLength: 25
         });
     },
@@ -89,20 +89,21 @@ qx.Class.define("client.InfoDialog",
         __input : 0,
         __input2 : 0,
         __combo : 0,
-        __nwselection : "MeetAndSpeak",
+        __nwselection : 'MeetAndSpeak',
         __winvisible : 0,
         __queue : [],
 
-        showInfoWin : function(topic, text, showok, callbackok, showno, callbackno, allowavoid)
+        showInfoWin : function(topic, text, showOk, callbackOk, showNo,
+                               callbackNo, alllowAvoid)
         {
-            if (this.__winvisible == 1) {
-                var obj = new Object();
+            if (this.__winvisible === 1) {
+                var obj = {};
                 obj.topic = topic;
                 obj.text = text;
-                obj.showok = showok;
-                obj.callbackok = callbackok;
-                obj.showno = showno;
-                obj.callbackno = callbackno;
+                obj.showOk = showOk;
+                obj.callbackOk = callbackOk;
+                obj.showNo = showNo;
+                obj.callbackNo = callbackNo;
 
                 this.__queue.push(obj);
                 return;
@@ -118,54 +119,55 @@ qx.Class.define("client.InfoDialog",
 
             this.__window.add(this.__box);
 
-            if (allowavoid == true) {
+            if (alllowAvoid === true) {
                 this.__box.add(this.__message4);
                 this.__message4.setValue(false);
             }
-            this.__box.add(this.__spacer, {flex: 1});
 
-            if (typeof(showok) != "undefined") {
-                this.__yesbutton.setLabel(showok);
+            this.__box.add(this.__spacer, { flex: 1 });
+
+            if (typeof(showOk) !== 'undefined') {
+                this.__yesbutton.setLabel(showOk);
                 this.__box.add(this.__yesbutton);
 
-                if (this.__yeslistenerid != 0) {
+                if (this.__yeslistenerid !== 0) {
                     this.__yesbutton.removeListenerById(this.__yeslistenerid);
                 }
 
-                this.__yeslistenerid = this.__yesbutton.addListener("execute", function(e) {
+                this.__yeslistenerid = this.__yesbutton.addListener(
+                    'execute', function(e) {
+                        if (typeof(callbackOk) !== 'undefined') {
+                            callbackOk();
+                        }
 
-                    if (typeof(callbackok) != "undefined") {
-                        callbackok();
-                    }
-
-                    if (this.__message4.getValue() == true) {
-                        this.settings.setShowCloseWarn(0);
-                    }
+                        if (this.__message4.getValue() === true) {
+                            this.settings.setShowCloseWarn(0);
+                        }
 
                     this.closeInfoWindow();
                 }, this);
             }
 
-            if (typeof(showno) != "undefined") {
-                this.__nobutton.setLabel(showno);
+            if (typeof(showNo) !== 'undefined') {
+                this.__nobutton.setLabel(showNo);
                 this.__box.add(this.__nobutton);
 
-                if (this.__nolistenerid != 0) {
+                if (this.__nolistenerid !== 0) {
                     this.__nobutton.removeListenerById(this.__nolistenerid);
                 }
 
-                this.__nolistenerid = this.__nobutton.addListener("execute", function(e) {
-                    if (typeof(callbackno) != "undefined") {
-                        callbackno();
-                    }
+                this.__nolistenerid = this.__nobutton.addListener(
+                    'execute', function(e) {
+                        if (typeof(callbackNo) !== 'undefined') {
+                            callbackNo();
+                        }
 
-                    if (this.__message4.getValue() == true) {
-                        this.settings.setShowCloseWarn(0);
-                    }
+                        if (this.__message4.getValue() === true) {
+                            this.settings.setShowCloseWarn(0);
+                        }
 
-                    this.closeInfoWindow();
-
-                }, this);
+                        this.closeInfoWindow();
+                    }, this);
             }
 
             this.__window.setModal(true);
@@ -182,8 +184,8 @@ qx.Class.define("client.InfoDialog",
 
             if (this.__queue.length > 0) {
                 var obj = this.__queue.shift();
-                this.showInfoWin(obj.topic, obj.text, obj.showok, obj.callbackok,
-                                 obj.showno, obj.callbackno);
+                this.showInfoWin(obj.topic, obj.text, obj.showOk,
+                                 obj.callbackOk, obj.showNo, obj.callbackNo);
             }
         },
 
@@ -194,21 +196,23 @@ qx.Class.define("client.InfoDialog",
 
             this.__window.add(this.__message);
 
-            if (mode == 0) {
-                this.__window.setCaption("Join existing group");
-                this.__message.setLabel("Type the name of the group you wish to join:");
+            if (mode === 0) {
+                this.__window.setCaption('Join existing group');
+                this.__message.setLabel(
+                    'Type the name of the group you wish to join:');
             } else {
-                this.__window.setCaption("Join IRC channel");
-                this.__message.setLabel("Type the name of the IRC channel you wish to join:");
+                this.__window.setCaption('Join IRC channel');
+                this.__message.setLabel(
+                    'Type the name of the IRC channel you wish to join:');
             }
 
-            this.__input.setValue("");
+            this.__input.setValue('');
             this.__window.add(this.__input);
 
-            this.__message3.setLabel("Password, if needed:");
+            this.__message3.setLabel('Password, if needed:');
             this.__window.add(this.__message3);
 
-            this.__input2.setValue("");
+            this.__input2.setValue('');
             this.__window.add(this.__input2);
 
             this.__window.add(this.__box2);
@@ -217,56 +221,58 @@ qx.Class.define("client.InfoDialog",
             this.__box.removeAll();
             this.__box2.removeAll();
 
-            this.__box.add(this.__spacer, {flex: 1});
-            this.__yesbutton.setLabel("OK");
+            this.__box.add(this.__spacer, { flex: 1 });
+            this.__yesbutton.setLabel('OK');
             this.__box.add(this.__yesbutton);
-            this.__nobutton.setLabel("Cancel");
+            this.__nobutton.setLabel('Cancel');
             this.__box.add(this.__nobutton);
 
-            if (mode == 1) {
-                this.__message2.setLabel("Network:");
+            if (mode === 1) {
+                this.__message2.setLabel('Network:');
 
                 this.__combo.removeAll();
                 //TODO: configuration system needed, now UPDATE THIS manually!
-                this.__combo.add(new qx.ui.form.ListItem("IRCNet"));
-                this.__combo.add(new qx.ui.form.ListItem("FreeNode"));
-                this.__combo.add(new qx.ui.form.ListItem("W3C"));
-                //this.__combo.add(new qx.ui.form.ListItem("W3C")); This one you just need to know
+                this.__combo.add(new qx.ui.form.ListItem('IRCNet'));
+                this.__combo.add(new qx.ui.form.ListItem('FreeNode'));
+                this.__combo.add(new qx.ui.form.ListItem('W3C'));
 
-                this.__combo.setValue("IRCNet");
-                this.__nwselection = "IRCNet";
+                this.__combo.setValue('IRCNet');
+                this.__nwselection = 'IRCNet';
 
-                this.__combo.addListener("changeValue", function(e) {
+                this.__combo.addListener('changeValue', function(e) {
                     this.__nwselection = e.getData();
                 }, this);
 
                 this.__box2.add(this.__message2);
                 this.__box2.add(this.__combo);
             } else {
-                this.__nwselection = "MeetAndSpeak";
+                this.__nwselection = 'MeetAndSpeak';
             }
 
-            if (this.__nolistenerid != 0) {
+            if (this.__nolistenerid !== 0) {
                 this.__nobutton.removeListenerById(this.__nolistenerid);
             }
 
-            this.__nolistenerid = this.__nobutton.addListener("execute", function(e) {
-                this.__window.close();
-            }, this);
+            this.__nolistenerid = this.__nobutton.addListener(
+                'execute', function(e) {
+                    this.__window.close();
+                }, this);
 
-            if (this.__yeslistenerid != 0) {
+            if (this.__yeslistenerid !== 0) {
                 this.__yesbutton.removeListenerById(this.__yeslistenerid);
             }
 
-            this.__yeslistenerid = this.__yesbutton.addListener("execute", function(e) {
-                var input = this.__input.getValue();
+            this.__yeslistenerid = this.__yesbutton.addListener(
+                'execute', function(e) {
+                    var input = this.__input.getValue();
 
-                if (input !== "") {
-                    this.rpc.call("JOIN", input + " " + this.__nwselection + " " +
-                                  this.__input2.getValue());
-                }
-                this.__window.close();
-            }, this);
+                    if (input !== '') {
+                        this.rpc.call('JOIN', input + ' ' + this.__nwselection +
+                                      ' ' + this.__input2.getValue());
+                    }
+
+                    this.__window.close();
+                }, this);
 
             rootItem.add(this.__window);
             this.__window.open();
@@ -279,12 +285,13 @@ qx.Class.define("client.InfoDialog",
             this.__window.removeAll();
             this.__box.removeAll();
 
-            this.__window.setCaption("Create new group");
-            this.__message.setLabel("Type the name of the group you wish to create:");
-            this.__message2.setLabel("Password (optional):");
+            this.__window.setCaption('Create new group');
+            this.__message.setLabel(
+                'Type the name of the group you wish to create:');
+            this.__message2.setLabel('Password (optional):');
 
             this.__window.add(this.__message);
-            this.__input.setValue("");
+            this.__input.setValue('');
             this.__window.add(this.__input);
 
             this.__window.add(this.__message2);
@@ -297,52 +304,56 @@ qx.Class.define("client.InfoDialog",
             this.__box2.removeAll();
 
             this.__box.add(this.__spacer, {flex: 1});
-            this.__yesbutton.setLabel("OK");
+            this.__yesbutton.setLabel('OK');
             this.__box.add(this.__yesbutton);
-            this.__nobutton.setLabel("Cancel");
+            this.__nobutton.setLabel('Cancel');
             this.__box.add(this.__nobutton);
 
 
-            if (this.__nolistenerid != 0) {
+            if (this.__nolistenerid !== 0) {
                 this.__nobutton.removeListenerById(this.__nolistenerid);
             }
 
-            this.__nolistenerid = this.__nobutton.addListener("execute", function(e) {
-                this.__window.close();
-            }, this);
+            this.__nolistenerid = this.__nobutton.addListener(
+                'execute', function(e) {
+                    this.__window.close();
+                }, this);
 
-            if (this.__yeslistenerid != 0) {
+            if (this.__yeslistenerid !== 0) {
                 this.__yesbutton.removeListenerById(this.__yeslistenerid);
             }
 
-            this.__yeslistenerid = this.__yesbutton.addListener("execute",
-                                                                function(e) {
-                                                                    this.__process();
-                                                                }, this);
+            this.__yeslistenerid = this.__yesbutton.addListener(
+                'execute', function(e) {
+                    this.__process();
+                }, this);
 
-            if (this.__inputlistenerid != 0) {
+            if (this.__inputlistenerid !== 0) {
                 this.__input.removeListenerById(this.__inputlistenerid);
             }
 
 
-            if (this.__input2listenerid != 0) {
+            if (this.__input2listenerid !== 0) {
                 this.__input2.removeListenerById(this.__input2listenerid);
             }
 
-            this.__inputlistenerid = this.__input.addListener("keypress", function(e) {
-                if (e.getKeyIdentifier() == "Enter")
-                {
-                    this.__process();
-                }
-            }, this);
+            this.__inputlistenerid = this.__input.addListener(
+                'keypress', function(e) {
+                    if (e.getKeyIdentifier() === 'Enter')
+                    {
+                        this.__process();
+                    }
+                }, this);
 
-            this.__input2listenerid = this.__input2.addListener("keypress", function(e) {
-                if (e.getKeyIdentifier() == "Enter") {
-                    this.__process();
-                }
+            this.__input2listenerid = this.__input2.addListener(
+                'keypress', function(e) {
+                    if (e.getKeyIdentifier() === 'Enter') {
+                        this.__process();
+                    }
             }, this);
 
             rootItem.add(this.__window);
+
             this.__window.open();
             this.__input.focus();
             this.__window.center();
@@ -353,8 +364,8 @@ qx.Class.define("client.InfoDialog",
             var input = this.__input.getValue();
             var input2 = this.__input2.getValue();
 
-            if (input !== "") {
-                this.rpc.call("CREATE", input + " " + input2);
+            if (input !== '') {
+                this.rpc.call('CREATE', input + ' ' + input2);
             }
             this.__window.close();
         }
