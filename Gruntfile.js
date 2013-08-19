f26068077937694b9ccb165fe862b61217da01ec
Ilkka Oksanen <iao@iki.fi>
2013-08-18 17:43:23 -0700

    Added first Grunt tasks

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        jshint: {
+            src: [ 'source/class/mas/*' ],
+            all: {
+            }
+        },
+
+        shell: {
+            devCycle: {
+                options: {
+                    stdout: true,
+                    stderr: true
+                },
+                command: 'vagrant ssh -c "sudo killall ralph.pl; cd /source/meetandspeak/ && sudo ./install.sh dev && sudo /opt/evergreen/ralph/ralph.pl -vn 1"'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-shell');
+
+    // Default task(s).
+    grunt.registerTask('default', [ 'jshint' ]);
+};
