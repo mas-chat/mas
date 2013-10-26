82d6069698e75f9db3ddeac627f931d3c4da0077
Ilkka Oksanen <iao@iki.fi>
2013-10-26 20:00:32 +0200

    Basic registration works
    Not all check are yet implemented.

--- server/public/javascripts/register.js
+++ server/public/javascripts/register.js
@@ -1,12 +1,33 @@
 
-$(function() {
-	$('#register-form').submit(function() {
-		$.post(
-			'/register',
-			$(this).serialize(),
-			function(data){
-				$("#results").html(data)
-			});
-		return false;
+require(["/vendor/javascripts/jqBootstrapValidation.js"], function(validation) {
+
+	$(function() {
+		$("input,select,textarea").not("[type=submit]").jqBootstrapValidation({
+			preventSubmit: true,
+			submitError: function($form, event, errors) {
+				alert("NOT OK");
+
+				event.preventDefault();
+            },
+            submitSuccess: function($form, event) {
+            	alert("OK");
+
+            	$.post('/register',
+            		$('#register-form').serialize(),
+            		function(data) {
+            			if (data.success == true) {
+            				//TBD
+            			} else {
+            				$('#error-msg').text(data.msg);
+            				$('#error-msg').css('visibility','visible')
+            			}
+            		});
+       
+            	event.preventDefault();
+            },
+            filter: function() {
+            	return $(this).is(":visible");
+            }
+        });
 	});
 });
