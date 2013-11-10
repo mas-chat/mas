5e31fc527e4371ba679a3baf17df0ba2681d195b
Ilkka Oksanen <iao@iki.fi>
2013-11-10 14:38:22 +0100

    Installed winston, reworked registration form

--- server/public/javascripts/register.js
+++ server/public/javascripts/register.js
@@ -2,32 +2,5 @@
 require(["/vendor/javascripts/jqBootstrapValidation.js"], function(validation) {
 
 	$(function() {
-		$("#register-form input, #register-form select, #register-form textarea").not("[type=submit]").jqBootstrapValidation({
-			preventSubmit: true,
-			submitError: function($form, event, errors) {
-				alert("NOT OK");
-
-				event.preventDefault();
-            },
-            submitSuccess: function($form, event) {
-            	alert("OK");
-
-            	$.post('/register',
-            		$('#register-form').serialize(),
-            		function(data) {
-            			if (data.success == true) {
-            				//TBD
-            			} else {
-            				$('#error-msg').text(data.msg);
-            				$('#error-msg').css('visibility','visible')
-            			}
-            		});
-       
-            	event.preventDefault();
-            },
-            filter: function() {
-            	return $(this).is(":visible");
-            }
-        });
 	});
 });
