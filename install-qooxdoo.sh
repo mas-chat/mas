8221dbe45761802120857ff373866b6bee8a0a9c
Ilkka Oksanen <iao@iki.fi>
2014-01-13 00:39:19 +0100

    Update server code to use generators

--- install-qooxdoo.sh
+++ install-qooxdoo.sh
@@ -3,10 +3,13 @@
 set -e
 
 mkdir tmp-install-qooxdoo
+mkdir -p vendor
+
 cd tmp-install-qooxdoo
 
 wget http://downloads.sourceforge.net/qooxdoo/qooxdoo-3.5-sdk.zip
 unzip qooxdoo-3.5-sdk.zip
-mv qooxdoo-3.5-sdk ../qooxdoo-sdk
+
+mv qooxdoo-3.5-sdk ../vendor/qooxdoo-sdk
 
 cd .. && rm -fr tmp-install-qooxdoo
