316ee9c29211995026611cd2b6868b5fb4f0a72d
Ilkka Oksanen <iao@iki.fi>
2014-01-12 20:14:13 +0100

    Switch from Express to Koa

--- server/server.js
+++ server/server.js
@@ -16,13 +16,15 @@
 
 w = require('winston');
 
-var express = require('express'),
-	exphbs  = require('express3-handlebars'),
+var koa = require('koa'),
+    router = require('koa-router'),
+    hbs = require('koa-hbs'),
+    less = require('koa-less'),
+    serve = require('koa-static'),
+    error = require('koa-error'),
 	routesIndex = require('./routes'),
     routesPages = require('./routes/pages'),
-    routesRegister = require('./routes/register'),
-	http = require('http'),
-	path = require('path');
+    path = require('path');
 
 var user = require('./middleware/User.js');
     chat = require('./lib/chat'),
@@ -32,56 +34,52 @@ var user = require('./middleware/User.js');
 w.add(w.transports.File, { filename: 'mas.log' });
 //w.remove(w.transports.Console);
 
-var app = express();
+var app = koa();
 
 w.info('Server starting.');
 
-// All environments
-app.set('port', process.env.PORT || 3000);
-
-app.set('views', __dirname + '/views');
-
-app.engine('handlebars', exphbs({
-	defaultLayout: 'main',
-	helpers: {
-		getPageJSFile: function (object) { return this.page + '.js'; }
-	}
-}));
-
-// Middlewares
-app.set('view engine', 'handlebars');
-
-app.use(express.favicon(path.join(__dirname, 'public/images/favicon.ico')));
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(express.cookieParser());
-app.use('/ralph', user());
-
-app.use(app.router);
-
-app.use(require('less-middleware')({ src: __dirname + '/public' }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/main', express.static( path.join(__dirname, '/../client')));
-app.use('/opt/qooxdoo', express.static('../qooxdoo-sdk'));
-app.use('/qooxdoo-sdk', express.static('../qooxdoo-sdk'));
-
 // Development only
-if (app.get('env') === 'development') {
-    app.use(express.errorHandler());
+if (app.env === 'development') {
+    app.use(error());
 }
 
+app.use(hbs.middleware({
+    defaultLayout: 'layouts/main',
+    viewPath: __dirname + '/views'
+}));
+
+hbs.registerHelper('getPageJSFile', function() {
+    return this.page + '.js';
+});
+
+app.use(router(app));
+
+//app.use(express.favicon(path.join(__dirname, 'public/images/favicon.ico')));
+//app.use(express.logger('dev'));
+//app.use(express.bodyParser());
+//app.use(express.methodOverride());
+//app.use(express.cookieParser());
+//app.use('/ralph', user());
+
+app.use(less(path.join(__dirname, 'public')));
+app.use(serve(path.join(__dirname, 'public')));
+
+//app.use('/main', express.static(path.join(__dirname, '/../client')));
+//app.use('/opt/qooxdoo', express.static('../qooxdoo-sdk'));
+//app.use('/qooxdoo-sdk', express.static('../qooxdoo-sdk'));
+
 // Rest API routes
-app.get('/ralph/:sessionId/:sendSeq/:timezone', chat.handleLongPoll);
-app.post('/login', login.handleLogin);
+//app.get('/ralph/:sessionId/:sendSeq/:timezone', chat.handleLongPoll);
+//app.post('/login', login.handleLogin);
+
+app.resource('register', require('./controllers/register'));
+
+// TBD: Use resource
+//app.get('/register.html', routesRegister);
+//app.post('/register', routesRegister);
 
 // Page routes
 app.get('/', routesIndex);
-app.get('/register.html', routesRegister);
-app.post('/register', routesRegister);
-app.get(/.html$/, routesPages); // Order matters
+app.get(/.html$/, routesPages); // All other pages
 
-http.createServer(app).listen(app.get('port'), function(){
-    console.log('Express server listening on port ' + app.get('port'));
-});
+app.listen(3000);
