d88dd011a7eda64a16375f101fd480d8cb23d5ea
Ilkka Oksanen <iao@iki.fi>
2014-01-27 21:14:56 +0100

    Update copyright

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,5 @@
 //
-//   Copyright 2009-2013 Ilkka Oksanen <iao@iki.fi>
+//   Copyright 2009-2014 Ilkka Oksanen <iao@iki.fi>
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
