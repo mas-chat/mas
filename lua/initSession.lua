b23d1dca524dc683b680cc7b0551dfaf10302e22
Ilkka Oksanen <iao@iki.fi>
2014-02-09 18:52:43 +0100

    Convert initSession function to lua

---
+++ lua/initSession.lua
@@ -0,0 +1,92 @@
+--
+--   Copyright 2014 Ilkka Oksanen <iao@iki.fi>
+--
+--   Licensed under the Apache License, Version 2.0 (the "License");
+--   you may not use this file except in compliance with the License.
+--   You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+--   Unless required by applicable law or agreed to in writing,
+--   software distributed under the License is distributed on an "AS
+--   IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+--   express or implied.  See the License for the specific language
+--   governing permissions and limitations under the License.
+--
+
+-- Initialization of outbox needs to be atomic operation (=Lua script)
+-- for streaming to be realiable.
+
+local outbox = KEYS[1]
+local sessionId = ARGV[1]
+local userId = ARGV[2]
+
+local function split(s, delimiter)
+    local result = {}
+    for match in (s..delimiter):gmatch('(.-)'..delimiter) do
+        table.insert(result, match)
+    end
+    return result
+end
+
+-- Redis HGETALL doesn't return a real hash, fix it.
+local hgetall = function (key)
+  local bulk = redis.call('HGETALL', key)
+    local result = {}
+    local nextkey
+    for i, v in ipairs(bulk) do
+        if i % 2 == 1 then
+            nextkey = v
+        else
+            result[nextkey] = v
+        end
+    end
+    return result
+end
+
+redis.call('LPUSH', outbox, cjson.encode({
+    ['id'] = 'SESSIONID',
+    ['sessionId'] = sessionId
+}))
+
+redis.call('LPUSH', outbox, cjson.encode({
+    ['id'] = 'SET',
+    ['settings'] = nil
+}))
+
+--Iterate through windows
+local windows = redis.call('SMEMBERS', 'windowlist:' .. userId);
+
+for i = 1, #windows do
+    local windowId, network, windowName = unpack(split(windows[i], ':'))
+    local window = hgetall('window:' .. userId .. ':' .. windowId);
+
+    redis.call('LPUSH', outbox, cjson.encode({
+        ['id'] = 'CREATE',
+        ['windowId'] = tonumber(windowId),
+        ['x'] = tonumber(window.x),
+        ['y'] = tonumber(window.y),
+        ['width'] = tonumber(window.width),
+        ['height'] = tonumber(window.height),
+        ['network'] = network,
+        ['chanName'] = windowName,
+        ['type'] = window.type,
+        ['sounds'] = 1, -- TBD
+        ['titlealert'] = 1, -- TBD
+        ['userMode'] = 2, -- TBD
+        ['visible'] = 1, -- TBD
+        ['newMsgs'] = 2, -- TBD
+        ['password'] = window.password,
+        ['topic'] = 'Hello' -- TBD
+    }))
+
+    local lines = redis.call('LRANGE', 'windowmsgs:' .. userId .. ':' .. windowId, 0, -1);
+
+    for ii = #lines, 1, -1 do
+        redis.call('LPUSH', outbox, lines[ii])
+    end
+end
+
+redis.call('LPUSH', outbox, cjson.encode({
+    ['id'] = 'INITDONE'
+}))
