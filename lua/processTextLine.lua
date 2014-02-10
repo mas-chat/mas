490ea8c860fed45ec298ffefff1908d9ffabc966
Ilkka Oksanen <iao@iki.fi>
2014-02-10 21:38:21 +0100

    Add new lua scripts

---
+++ lua/processTextLine.lua
@@ -0,0 +1,27 @@
+--
+--   Copyright 2014 Ilkka Oksanen <iao@iki.fi>
+--
+--   Licensed under the Apache License, Version 2.0 (the "License");
+--   you may not use this file except in compliance with the License.
+--   You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+--   Unless required by applicable law or agreed to in writing,
+--   software distributed under the License is distributed on an "AS
+--   IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+--   express or implied.  See the License for the specific language
+--   governing permissions and limitations under the License.
+--
+
+local userId = ARGV[1]
+local windowId = ARGV[2]
+local command = ARGV[3]
+
+redis.call('LPUSH', 'windowmsgs:' .. userId .. ':' .. windowId, command)
+
+local sessions = redis.call('SMEMBERS', 'sessionlist:' .. userId)
+
+for i = 1, #sessions do
+    redis.call('LPUSH', 'outbox:' .. userId .. ':' .. sessions[i], command)
+end
