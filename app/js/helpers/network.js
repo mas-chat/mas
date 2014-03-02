ecd1341a7cab718fd2a855f4eb74ea731792410d
Ilkka Oksanen <iao@iki.fi>
2014-03-02 02:21:56 +0100

    Show incoming messages properly

--- app/js/helpers/network.js
+++ app/js/helpers/network.js
@@ -16,14 +16,20 @@
 
 'use strict';
 
+require('./commandParser');
+
 App.Network = Ember.Object.extend({
     _sessionId: 0,
     _pollSeq: 0,
     _sendSeq: 0,
     _firstPoll: true,
+    _parser: null,
 
     init: function() {
         this._pollMsgs();
+        this._parser = App.CommandParser.create({
+            store: this.get('store')
+        });
     },
 
     _pollMsgs: function() {
@@ -39,14 +45,45 @@ App.Network = Ember.Object.extend({
         $.ajax({
             dataType: 'json',
             url: '/api/v1/listen/' + this._sessionId + '/' + this._pollSeq + tz,
-            success: this._processResponse,
-            error: this._handleFailure,
+            success: this._pollMsgsSuccess,
+            error: this._pollMsgsFailure,
+            context: this,
             timeout: 35000
         });
     },
 
-    _processResponse: function(data) {
-        Ember.Logger.info('Got good response' + data);
+    _pollMsgsSuccess: function(data) {
+        this._pollSeq++;
+
+        if (this._firstPoll) {
+            this._firstPoll = false;
+        }
+
+        Ember.Logger.info('<-- Response to polling request');
+
+        this._processCommands(data, false);
+        this._pollMsgs();
+    },
+
+    _processCommands: function(commands, solicited) {
+        for (var i = 0; i < commands.length; i++) {
+            var command = commands[i];
+
+            var prefix = '<-- MSG: ';
+            if (!solicited) {
+                // This is a response to polling request, it can contain
+                // multiple commands
+                prefix = '  |- MSG: ';
+            }
+
+            Ember.Logger.info(prefix + JSON.stringify(command));
+
+            if (command.id === 'SESSIONID') {
+                this._sessionId = command.sessionId;
+            } else {
+                this._parser.process(command);
+            }
+        }
     },
 
     _handleFailure: function(jqxhr, textStatus, error ) {
