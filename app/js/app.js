68f3d9134aaf86814dbb8882d7a46efba4daf068
Ilkka Oksanen <iao@iki.fi>
2014-03-09 15:55:20 +0100

    Update window arrange logic

--- app/js/app.js
+++ app/js/app.js
@@ -23,6 +23,7 @@ require('./views/application');
 require('./views/window');
 require('./controllers/main');
 require('./controllers/window');
+require('./controllers/application');
 require('./models/message');
 require('./models/window');
 require('./helpers/network');
