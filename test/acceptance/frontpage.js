041ce36fca45be566fd39ba00a0a20ec02132a28
Ilkka Oksanen <iao@iki.fi>
2014-05-29 16:15:04 +0200

    Improve first functional test

--- test/acceptance/frontpage.js
+++ test/acceptance/frontpage.js
@@ -1,7 +1,10 @@
+require('./helper.js');
+
 casper.test.begin('Load frontpage', 2, function suite(test) {
-    casper.start("http://localhost:44199/", function() {
-        test.assertTitle("MAS -", "MAS homepage title is the one expected");
-        test.assertExists('#mas-logo', "main logo is found");
+    casper.start('http://localhost:44199/', function() {
+        test.assertTitle('MAS -', 'MAS homepage title is the one expected');
+        test.assertExists('#mas-logo', 'main logo is found');
+        this.capture('screenshot.png');
     });
 
     casper.run(function() {
