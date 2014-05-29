1a8b997fff06ea16ea1ce4b33b208caf2e7cd481
Ilkka Oksanen <iao@iki.fi>
2014-05-29 17:09:36 +0200

    Add first registration tests

--- test/acceptance/test-frontpage.js
+++ test/acceptance/test-frontpage.js
@@ -1,10 +1,9 @@
-require('./helper.js');
+require('./helper');
 
 casper.test.begin('Load frontpage', 2, function suite(test) {
     casper.start('http://localhost:44199/', function() {
         test.assertTitle('MAS -', 'MAS homepage title is the one expected');
         test.assertExists('#mas-logo', 'main logo is found');
-        this.capture('screenshot.png');
     });
 
     casper.run(function() {
