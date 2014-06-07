510c0bc8773576029e2e82acc09c45a729e6962a
Ilkka Oksanen <iao@iki.fi>
2014-06-07 19:33:52 +0200

    Enable slimerjs in Travis

---
+++ scripts/travis-install-slimerjs.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+sh -e /etc/init.d/xvfb start
+echo 'Installing SlimerJS'
+
+wget http://download.slimerjs.org/v0.9/0.9.1/slimerjs-0.9.1.zip
+
+unzip slimerjs-0.9.1.zip
+mkdir bin
+mv slimerjs-0.9.1 ./bin/slimerjs
