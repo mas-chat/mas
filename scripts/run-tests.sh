1d99079647240bbef50b6d86b31ef819eeb4d511
Ilkka Oksanen <iao@iki.fi>
2014-06-07 20:25:06 +0200

    Also redirect stderr

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -12,7 +12,7 @@ PID=$!
 echo "Waiting 1 second for the redis to start."
 sleep 1
 
-nohup node --harmony ./server/server.js --configFile=test/mas-test.conf > server.log &
+nohup node --harmony ./server/server.js --configFile=test/mas-test.conf > server.log 2> server_error.log&
 FE_PID=$!
 
 trap "kill -9 $PID; kill -9 $FE_PID" EXIT
