accb33a94b2c45ecc76e8cde14a6458e8cdf1ae3
Ilkka Oksanen <iao@iki.fi>
2014-09-18 21:06:47 +0000

    Fix IRC mode command handling

--- server/backends/irc/controller.js
+++ server/backends/irc/controller.js
@@ -613,7 +613,8 @@ function *handleMode(userId, msg) {
 
     yield textLine.sendByWindowId(userId, windowId, {
         cat: 'info',
-        body: 'Mode change: ' + msg.params.join(' ') + ' by ' + msg.nick ? msg.nick : msg.serverName
+        body: 'Mode change: ' + msg.params.join(' ') + ' by ' +
+            (msg.nick ? msg.nick : msg.serverName)
     });
 
     var modeParams = msg.params.slice(1);
