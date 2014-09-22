c94d094368006fce79690bd0dfac8ec0cd51720d
Ilkka Oksanen <iao@iki.fi>
2014-09-22 08:00:38 +0200

    Wait little before exiting to log exit reason

--- server/lib/conf.js
+++ server/lib/conf.js
@@ -51,7 +51,9 @@ exports.get = function(key) {
 
     if (value === undefined) {
         log.error('Config variable missing in the config file: ' + key);
-        process.exit(1);
+        setTimeout(function() {
+            process.exit(1);
+        }, 5000);
     }
 
     return value;
