eafc94236d383af88b6698d2be7bd0d0b28912e2
Ilkka Oksanen <iao@iki.fi>
2014-10-06 21:57:19 +0200

    Improve masctl.js

--- scripts/masctl.js
+++ scripts/masctl.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node --harmony
+#!/usr/bin/env node
 //
 //   Copyright 2014 Ilkka Oksanen <iao@iki.fi>
 //
@@ -17,61 +17,176 @@
 
 'use strict';
 
-var exec = require('child_process').exec,
+var child = require('child_process'),
     fs = require('fs'),
     path = require('path'),
     semver = require('semver'),
-    yargs = require('yargs');
+    yargs = require('yargs'),
+    nconf = require('nconf');
+
+require('colors');
 
 var argv = yargs
     .usage('Usage: $0 start|stop|restart|status')
-    .demand(1)
-    .alias('i', 'import')
-    .describe('i', 'MeetAndSpeak.com specific option.')
-    .default('i', false)
+    .demand(1, 'Action (start, stop, restart, or status) missing.')
     .alias('f', 'configFile')
     .describe('f', 'Configuration file.')
     .default('f', 'mas.conf')
+    .boolean('c')
     .alias('c', 'connman')
     .describe('c', 'Also start, stop or restart IRC connman process.')
     .default('c', false)
+    .boolean('b')
+    .alias('b', 'background')
+    .describe('b', 'Daemonize MAS processes')
+    .default('b', false)
     .help('h')
     .alias('h', 'help')
     .version('0.0.1', 'v')
     .alias('v', 'version')
     .argv;
 
-if (semver.lt(process.version, 'v0.11.11')) {
-    console.log('ERROR: Installed Node.js version must be at least v0.11.11');
-    process.exit(1);
+checkNodeVersion();
+setupNconf();
+
+var action = argv._[0];
+var env = getConfigOption('common:env');
+
+console.log('MAS env: ' + env.yellow);
+
+var processes = {
+    'mas-frontend': 'server.js',
+    'mas-irc': 'backends/irc/controller.js',
+    'mas-loopback': 'backends/loopback/controller.js'
+};
+
+if (argv.connman === true || action === 'status') {
+    processes['irc-connman'] = 'backends/irc/connectionManager.js';
 }
 
-var rootPath = path.join(__dirname, '..');
-var configFile = path.join(rootPath, argv.configFile);
+var pidDir = getConfigOption('pid:directory');
 
-if (!fs.existsSync(configFile)) {
-    console.log('ERROR: Main configuration file: ' + configFile + ' is missing.');
-    process.exit(1);
+if (action === 'stop' || action === 'restart') {
+    // Kill all running MAS processes
+    for (var component in processes) {
+        var pidFile = path.join(getAbsolutePath(pidDir), component + '-' + env + '.pid');
+
+        if (!fs.existsSync(pidFile)) {
+            continue;
+        }
+
+        var pid = parseInt(fs.readFileSync(pidFile));
+
+        try {
+            process.kill(pid);
+        } catch(e) {
+            console.log('Process ' + component + ' (pid: ' + pid +
+                ') was not running even the PID file existed.');
+            fs.unlinkSync(pidFile);
+        }
+
+        console.log('Stopped:'.red + ' ' + component);
+    }
 }
 
-var processes = [
-    'server.js',
-    'backends/irc/controller.js',
-    'backends/loopback/controller.js'
-];
+if (action === 'start' || action === 'restart') {
+    for (var component in processes) {
+        var spawnOptions = {
+            detached: argv.background,
+            stdio: argv.background ? 'ignore' : null
+        };
 
-processes.forEach(function(component) {
-    var child = exec('node --harmony ' + path.join(rootPath, 'server', component));
+        var commandLineParameters = [
+            '--harmony',
+            getAbsolutePath(path.join('server', processes[component])),
+            '--configFile',
+            argv.configFile
+        ];
 
-    child.stdout.on('data', prettyPrint);
-    child.stderr.on('data', prettyPrint);
+        var node = child.spawn('node', commandLineParameters, spawnOptions);
 
-    child.on('close', function(code) {
-        console.log('Exit code: ' + code);
-        process.exit(1);
-    });
-});
+        if (argv.background) {
+             node.unref();
+        } else {
+            node.stdout.on('data', prettyPrint);
+            node.stderr.on('data', prettyPrint);
+            node.on('close', handleClose);
+        }
+
+        console.log('Starting:'.green + ' ' + component);
+    }
+}
+
+if (action === 'status') {
+    for (var component in processes) {
+        var pidFile = path.join(getAbsolutePath(pidDir), component + '-' + env + '.pid');
+        var alive = false;
+        var pid;
+
+        if (fs.existsSync(pidFile)) {
+            pid = parseInt(fs.readFileSync(pidFile));
+
+            try {
+                process.kill(pid, 0);
+                alive = true;
+            } catch(e) { }
+        }
+
+        if (alive) {
+            console.log(component + ': ' + 'running'.green + ', pid: ' + pid);
+        } else {
+            console.log(component + ': ' + 'stopped'.red);
+        }
+    }
+}
 
 function prettyPrint(text) {
     process.stdout.write(text);
+}
+
+function handleClose(code) {
+    console.log('Exit code: ' + code);
+    process.exit(1);
+}
+
+function checkNodeVersion() {
+    if (semver.lt(process.version, 'v0.11.11')) {
+        console.log('ERROR: Installed Node.js version must be at least v0.11.11');
+        process.exit(1);
+    }
+}
+
+function setupNconf() {
+    var configFile = getAbsolutePath(argv.configFile);
+
+    if (!fs.existsSync(configFile)) {
+        console.error('ERROR: '.red + 'Config file ' + configFile + ' missing.');
+        process.exit(1);
+    }
+
+    nconf.use('file', {
+        file: configFile,
+        format: nconf.formats.ini
+    });
+}
+
+function getConfigOption(key) {
+    var value = nconf.get(key);
+
+    if (value === undefined) {
+        console.error('ERROR: '.red + 'Config variable missing in the config file: ' + key);
+        process.exit(1);
+    }
+
+    return value;
+}
+
+function getAbsolutePath(origPath) {
+    var ROOTPATH = path.join(__dirname, '..');
+
+    if (origPath.charAt(0) === path.sep) {
+        return path.normalize(origPath); // Absolute path
+    } else {
+        return path.join(ROOTPATH, origPath);
+    }
 }
