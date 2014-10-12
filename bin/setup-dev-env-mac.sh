5644eaccc627ba0707c4c11303159a4f2962b7c4
Ilkka Oksanen <iao@iki.fi>
2014-10-12 13:17:28 +0200

    Improve dev script

--- bin/setup-dev-env-mac.sh
+++ bin/setup-dev-env-mac.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+ROOT=$( cd $( dirname "${BASH_SOURCE[0]}" ) && cd .. && pwd )
+REDIS_PORT=6379
+
+cd $ROOT
+
 echo 'Making sure Redis is running...'
 redis-server ./conf/redis.conf --port $REDIS_PORT
 
@@ -8,5 +13,7 @@ sudo sysctl -w kern.maxfilesperproc=50000
 sudo sysctl -w kern.ipc.somaxconn=50000
 
 ulimit -S -n 50000
+
+killall ircd
 
 ircd
