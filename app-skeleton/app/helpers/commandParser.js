c40405b1f9f99186a43cf564c0f45af01b920f44
Ilkka Oksanen <iao@iki.fi>
2014-11-17 22:18:27 +0100

    Initial ES6 module conversion

---
+++ app-skeleton/app/helpers/commandParser.js
@@ -0,0 +1,158 @@
+//
+//   Copyright 2009-2014 Ilkka Oksanen <iao@iki.fi>
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing,
+//   software distributed under the License is distributed on an "AS
+//   IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+//   express or implied.  See the License for the specific language
+//   governing permissions and limitations under the License.
+//
+
+'use strict';
+
+import Ember from 'ember';
+
+export default Ember.Object.extend({
+    process: function(command) {
+        var name = command.id;
+        var targetWindow = null;
+        var windowId = command.windowId;
+
+        delete command.id;
+
+        if (typeof windowId === 'number') {
+            targetWindow = Mas.windowCollection.findBy('windowId', windowId);
+        }
+
+        var funcName = '_handle' + name.charAt(0) + name.substring(1).toLowerCase();
+
+        if (!this[funcName]) {
+            Ember.Logger.warn('Unknown command received: ' + name);
+        } else {
+            this[funcName](command, targetWindow);
+        }
+    },
+
+    _handleCreate: function(data) {
+        var windowRecord = Mas.Window.create(data);
+        Mas.windowCollection.pushObject(windowRecord);
+    },
+
+    _handleClose: function(data, targetWindow) {
+        Mas.windowCollection.removeObject(targetWindow);
+    },
+
+    _handleAddtext: function(data, targetWindow) {
+        if (!targetWindow) {
+            return;
+        }
+
+        delete data.windowId;
+        var messageRecord = Mas.Message.create(data);
+        var messages = targetWindow.messages;
+
+        if (messages.length > 200) {
+            messages.shiftObject();
+            targetWindow.deletedLine = true;
+        }
+
+        messages.pushObject(messageRecord);
+    },
+
+    _handleNick: function(data) {
+        jQuery.extend(Mas.nicks, data);
+    },
+
+    _handleInitdone: function() {
+        // INITDONE command usually arrives together with another commands. These
+        // other commands update property bindings. INITDONE triggers code that
+        // assumes these updates have been processed. Therefore INITDONE must be
+        // processed one run loop round later than everything else.
+        Ember.run.next(this, function() {
+            Mas.__container__.lookup('controller:application').set('initDone', true);
+        });
+    },
+
+    _handleUsers: function(data) {
+        for (var userId in data.mapping) { /* jshint -W089 */
+            var user = data.mapping[userId];
+            Mas.userDb.set('users.' + userId, user);
+        }
+    },
+
+    _handleAddmembers: function(data, targetWindow) {
+        if (!targetWindow) {
+            return;
+        }
+
+        if (data.reset) {
+            targetWindow.operators.clear();
+            targetWindow.voices.clear();
+            targetWindow.users.clear();
+        }
+
+        data.members.forEach(function(member) {
+            var userId = member.userId;
+            this._removeUser(userId, targetWindow);
+
+            switch (member.role) {
+                case '@':
+                    targetWindow.operators.pushObject(userId);
+                    break;
+                case '+':
+                    targetWindow.voices.pushObject(userId);
+                    break;
+                default:
+                    targetWindow.users.pushObject(userId);
+                    break;
+            }
+        }.bind(this));
+    },
+
+    _handleDelmembers: function(data, targetWindow) {
+        if (!targetWindow) {
+            return;
+        }
+
+        data.members.forEach(function(member) {
+            this._removeUser(member.userId, targetWindow);
+        }.bind(this));
+    },
+
+    _handleUpdate: function(data, targetWindow) {
+        if (!targetWindow) {
+            return;
+        }
+
+        targetWindow.setProperties(data);
+    },
+
+    _handleFriends: function(data) {
+        Mas.friendCollection.clear();
+
+        data.friends.forEach(function(friend) {
+            var friendRecord = Mas.Friend.create(friend);
+            Mas.friendCollection.pushObject(friendRecord);
+        }.bind(this));
+    },
+
+    _handleFriendsupdate: function(data) {
+        var friend = Mas.friendCollection.findBy('userId', data.userId);
+        friend.set('online', data.online);
+        if (data.last) {
+            friend.set('last', data.last);
+        }
+    },
+
+    _removeUser: function(userId, targetWindow) {
+        targetWindow.operators.removeObject(userId);
+        targetWindow.voices.removeObject(userId);
+        targetWindow.users.removeObject(userId);
+    }
+});
---
+++ app-skeleton/app/views/grid.js
@@ -0,0 +1,123 @@
+//
+//   Copyright 2009-2014 Ilkka Oksanen <iao@iki.fi>
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing,
+//   software distributed under the License is distributed on an "AS
+//   IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+//   express or implied.  See the License for the specific language
+//   governing permissions and limitations under the License.
+//
+
+'use strict';
+
+import Ember from 'ember';
+
+var _ = require('lodash/dist/lodash');
+
+export default Ember.View.extend({
+    classNames: [ 'grid', 'flex-1', 'flex-grow-row' ],
+
+    PADDING: 5,
+
+    didInsertElement: function() {
+        $(window).on('resize', Ember.run.bind(this, function() {
+            this.layoutWindows(false);
+        }));
+    },
+
+    windowAdded: function(animate) {
+        if (this.get('controller.initDone')) {
+            Ember.run.next(this, function() { this.layoutWindows(animate); });
+        }
+    },
+
+    initReady: function() {
+        Ember.run.next(this, function() { this.layoutWindows(false); });
+    }.observes('controller.initDone'),
+
+    layoutWindows: function(animate) {
+        var that = this;
+        var duration = animate ? 600 : 0;
+        var el = this.get('element');
+        var container = this._containerDimensions();
+        var expandedWindow = el.querySelector('.window.expanded');
+
+        if (expandedWindow) {
+            $(expandedWindow).velocity({
+                left: this.PADDING + 'px',
+                top: this.PADDING + 'px',
+                width: container.width - 2 * this.PADDING + 'px',
+                height: container.height - 2 * this.PADDING + 'px'
+            }, duration);
+            return;
+        }
+
+        var windows = el.querySelectorAll('.window.visible');
+        var rowNumbers = _.uniq(_.map(windows,
+            function(element) { return element.getAttribute('data-row'); }));
+        var rowHeight = (container.height - (rowNumbers.length + 1) * this.PADDING) /
+            rowNumbers.length;
+
+        _.forEach(rowNumbers, function(row, rowIndex) {
+            var windowsInRow = el.querySelectorAll('.window.visible[data-row="' + row + '"]');
+            var windowWidth = (container.width - (windowsInRow.length + 1) * that.PADDING) /
+                windowsInRow.length;
+
+            _.forEach(windowsInRow, function(element, index) {
+                that._animate(element, index, rowIndex, windowWidth, rowHeight, duration);
+            });
+        });
+    },
+
+    _animate: function(el, columnIndex, rowIndex, width, height, duration) {
+        var $el = $(el);
+        var position = $el.position();
+
+        var dim = {
+            currentLeft: position.left,
+            currentTop: position.top,
+            currentWidth: $el.width(),
+            currentHeight: $el.height(),
+            left: columnIndex * width + (columnIndex + 1) * this.PADDING,
+            top: rowIndex * height + (rowIndex + 1) * this.PADDING,
+            width: width,
+            height: height
+        };
+
+        dim = _.mapValues(dim, function(val) { return Math.round(val); });
+
+        if (dim.left === dim.currentLeft && dim.top === dim.currentTop &&
+            dim.width === dim.currentWidth && dim.height === dim.currentHeight) {
+            // Nothing to animate
+            return;
+        }
+
+        $(el).velocity('stop').velocity({
+            left:  dim.left + 'px',
+            top:  dim.top + 'px',
+            width: dim.width + 'px',
+            height: dim.height + 'px'
+        }, {
+            duration: duration,
+            visibility: 'visible',
+            complete: function() {
+                // Make sure window shows the latest messages
+                var view = Ember.View.views[$el.attr('id')];
+                Ember.run.next(view, view.layoutDone);
+            }
+        });
+    },
+
+    _containerDimensions: function() {
+        return {
+            width: this.$().width(),
+            height: this.$().height()
+        };
+    }
+});
