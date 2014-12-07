39764eeabcd983e31debdf189d1724da58a12217
Ilkka Oksanen <iao@iki.fi>
2014-12-07 18:14:52 +0100

    Fix spinner image path

--- client/app/models/message.js
+++ client/app/models/message.js
@@ -54,7 +54,7 @@ export default Ember.Object.extend({
 
             if (imgSuffixes.indexOf(urlObj.suffix()) !== -1) {
                 imgUrls.push('<li><a href="' + this._escHtml(url, true) + '" class="user-img">' +
-                    '<img src="/images/client/spinner_96.gif" data-src="' +
+                    '<img src="/app/assets/images/spinner_96.gif" data-src="' +
                     this._escHtml(url, true) + '"></a></li>');
                 visibleLink = this._escHtml(urlObj.filename(), false);
             } else {
