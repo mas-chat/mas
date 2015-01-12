79ba569c1da30df56ad3fc24bcb28b899282601c
Ilkka Oksanen <iao@iki.fi>
2015-01-12 20:09:57 +0100

    Fix integration tests

--- test/integration/test-send-message-irc.js
+++ test/integration/test-send-message-irc.js
@@ -79,7 +79,7 @@ casper.test.begin('Register new user', 9, function suite(test) {
         this.clickLabel('OK');
     });
 
-    casper.waitForText('has joined channel', function() {
+    casper.waitForText('has joined the group', function() {
         this.fillSelectors('.window form', {
             '.form-control': 'Works finally!'
         }, true);
