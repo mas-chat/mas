5fbbef4ce5a521a8d3fcfe90c39712af029cc198
Ilkka Oksanen <iao@iki.fi>
2015-01-19 20:30:49 +0100

    Fix port used in browser tests

--- test/browser/tests/basic/frontpage_test.js
+++ test/browser/tests/basic/frontpage_test.js
@@ -2,7 +2,7 @@
 module.exports = {
     "Load front page" : function (browser) {
         browser
-            .url("http://localhost:3200")
+            .url("http://localhost:44199")
             .waitForElementVisible('body', 1000)
             .assert.containsText('.login', 'Sign in')
             .end();
