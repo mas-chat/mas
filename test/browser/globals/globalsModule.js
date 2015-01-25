16b9152e28565ee7dff9b516750a78e00f4a7fb2
Ilkka Oksanen <iao@iki.fi>
2015-01-25 18:51:38 +0100

    Run selenium test one by one, not parallel
    Also make sure redis db is cleared between test cases

--- test/browser/globals/globalsModule.js
+++ test/browser/globals/globalsModule.js
@@ -1,9 +1,23 @@
 
-var request = require('superagent');
+var request = require('superagent'),
+    co = require('co'),
+    redisModule = require('../../../server/lib/redis'),
+    redis = redisModule.createClient();
 
 module.exports = {
     local: {
         dontReportSauceLabs: true
+    },
+
+    before: function(done) {
+        co(function*() {
+            // Clear redis database
+            yield redis.flushdb();
+            yield redis.quit();
+
+            yield redisModule.initDB();
+            done();
+        })();
     },
 
     reporter: function(results, done) {
