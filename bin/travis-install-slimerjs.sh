a4d4b40b2b1d3e4035b7a22f572e51027246b044
Ilkka Oksanen <iao@iki.fi>
2015-01-25 09:45:58 +0100

    Remove obsolete scripts

--- bin/travis-install-slimerjs.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-sh -e /etc/init.d/xvfb start
-echo 'Installing SlimerJS'
-
-wget http://download.slimerjs.org/v0.9/0.9.3/slimerjs-0.9.3.zip
-
-unzip slimerjs-0.9.3.zip
-mv slimerjs-0.9.3 ./slimerjs
