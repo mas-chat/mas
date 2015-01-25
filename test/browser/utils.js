3ce089b5b66ee5998da82b8b68ce2af788e07b95
Ilkka Oksanen <iao@iki.fi>
2015-01-25 23:10:40 +0100

    Switch back to vanilla Nightwatch.js and add missing file

---
+++ test/browser/utils.js
@@ -0,0 +1,41 @@
+
+var request = require('superagent');
+
+exports.tearDown = function(done) {
+    console.log(this)
+
+    var passedStatus = this.results.failed === 0 && this.results.errors === 0;
+    console.log('Final result: "{ passed: ' + passedStatus + ' }"');
+
+    if (!this.client.globals.dontReportSauceLabs) {
+        var userName = this.client.options.username;
+        var accessKey = this.client.options.accessKey;
+        var sessionId =this.client.sessionId;
+        var baseUrl = 'https://saucelabs.com/rest/v1/';
+
+        console.log('Sending final result to Saucelabs...');
+
+        if (userName && accessKey) {
+            request
+            .put(baseUrl + userName + '/jobs/' + sessionId)
+            .send({ passed: passedStatus })
+            .auth(userName, accessKey)
+            .end(function(error, res){
+                if (error) {
+                    console.log('ERROR sending verdict');
+                    console.log(error);
+                } else {
+                    console.log('Verdict sent to Sauce Labs, response:' +
+                        res.res.statusMessage);
+                }
+                done();
+            });
+        } else {
+            console.log('Username or access key missing, username: ' + userName);
+            done();
+        }
+    } else {
+        console.log('Local testing. Not notifying Saucelabs.');
+        done();
+    }
+};
