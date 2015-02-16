a0da67a1431af7a043431cea6ecf64f4ac3abbf3
Ilkka Oksanen <iao@iki.fi>
2015-02-16 19:27:37 +0100

    Add elasticsearch to dev script

--- bin/setup-dev-env-mac.sh
+++ bin/setup-dev-env-mac.sh
@@ -8,6 +8,9 @@ cd $ROOT
 echo 'Making sure Redis is running...'
 redis-server ./conf/redis.conf --port $REDIS_PORT
 
+echo 'Making sure Elasticsearch is running...'
+elasticsearch -d --config=/usr/local/opt/elasticsearch/config/elasticsearch.yml
+
 sudo sysctl -w kern.maxfiles=50000
 sudo sysctl -w kern.maxfilesperproc=50000
 sudo sysctl -w kern.ipc.somaxconn=50000
