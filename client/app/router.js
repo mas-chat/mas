3dfb69b903f50cabd4cfb6d02916c274a387329f
Ilkka Oksanen <iao@iki.fi>
2015-04-02 12:17:49 +0200

    Upgrade to ember-cli 0.2.2

--- client/app/router.js
+++ client/app/router.js
@@ -20,11 +20,9 @@ import Ember from 'ember';
 import config from './config/environment';
 
 let Router = Ember.Router.extend({
-  location: config.locationType
+    location: config.locationType
 });
 
-Router.map(function() {
+export default Router.map(function() {
     this.route('index', { path: '/app/' });
 });
-
-export default Router;
