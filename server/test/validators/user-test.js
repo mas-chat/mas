64ba6306e8c4f99902d3bffe353433f9036b19b8
Ilkka Oksanen <iao@iki.fi>
2016-01-30 15:19:04 +0100

    Add user model validator

---
+++ server/test/validators/user-test.js
@@ -0,0 +1,90 @@
+'use strict';
+
+const chai = require("chai"),
+      expect = require('chai').expect,
+      userValidator = require('../../validators/user.js');
+
+describe('Validation of ', () => {
+    it('name, email, and password fails', () => {
+        const res = userValidator.validate({
+            name: 'Ilkka',
+            email: 'foobar',
+            password: '12345'
+        });
+
+        expect(res).to.deep.equal({
+            valid: false,
+            errors: {
+                email: 'Please enter a valid email address.',
+                name: 'Please enter at least 6 characters.',
+                password: 'Please enter at least 6 characters.'
+            }
+        });
+    });
+
+    it('nick fails because too short', () => {
+        const res = userValidator.validate({
+            nick: 'io'
+        });
+
+        expect(res).to.deep.equal({
+            valid: false,
+            errors: {
+                nick: 'Nick has to be 3-15 characters long.'
+            }
+        });
+    });
+
+    it('nick fails because too long', () => {
+        const res = userValidator.validate({
+            nick: 'a234567890123456'
+        });
+
+        expect(res).to.deep.equal({
+            valid: false,
+            errors: {
+                nick: 'Nick has to be 3-15 characters long.'
+            }
+        });
+    });
+
+    it('nick fails because of starting digit', () => {
+        const res = userValidator.validate({
+            nick: '2ilkka'
+        });
+
+        expect(res).to.deep.equal({
+            valid: false,
+            errors: {
+                nick: 'Nick can\'t start with digit'
+            }
+        });
+    });
+
+    it('nick fails because of illegal character', () => {
+        const res = userValidator.validate({
+            nick: 'il#kka'
+        });
+
+        expect(res).to.deep.equal({
+            valid: false,
+            errors: {
+                nick: 'Illegal characters, allowed are a-z, 0-9, [, ], \\, `, _, ^, {, |, }'
+            }
+        });
+    });
+
+    it('all fields succeeds', () => {
+        const res = userValidator.validate({
+            name: 'IlkkaO',
+            nick: 'ilkka_',
+            email: 'iao@iki.fi',
+            password: '123456'
+        });
+
+        expect(res).to.deep.equal({
+            valid: true,
+            errors: {}
+        });
+    });
+});
---
+++ server/validators/user.js
@@ -0,0 +1,78 @@
+//
+//   Copyright 2009-2016 Ilkka Oksanen <iao@iki.fi>
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing,
+//   software distributed under the License is distributed on an "AS
+//   IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+//   express or implied.  See the License for the specific language
+//   governing permissions and limitations under the License.
+//
+
+'use strict';
+
+let validator = new UserValidator();
+
+exports.validate = function(props) {
+    let errors = {};
+
+    for (const prop in props) {
+        const validate = validator[prop];
+
+        if (validate) {
+            const { valid, error } = validate(props[prop]);
+
+            if (!valid) {
+                errors[prop] = error;
+            }
+        }
+    }
+
+    return { valid: Object.keys(errors).length === 0, errors: errors };
+}
+
+function UserValidator() {}
+
+UserValidator.prototype.name = function(name) {
+    if (!name || name.length < 6) {
+        return { valid: false, error: 'Please enter at least 6 characters.' };
+    }
+
+    return { valid: true };
+}
+
+UserValidator.prototype.email = function(email) {
+    if (!email || email.indexOf('@') === -1) {
+        return { valid: false, error: 'Please enter a valid email address.' };
+    }
+
+    return { valid: true };
+}
+
+UserValidator.prototype.password = function(password) {
+    if (!password || password.length < 6) {
+        return { valid: false, error: 'Please enter at least 6 characters.' };
+    }
+
+    return { valid: true };
+}
+
+UserValidator.prototype.nick = function(nick) {
+    if (!nick) {
+        return { valid: false, error: 'Please enter a nick' };
+    } else if (nick.length < 3 || nick.length > 15) {
+        return { valid: false, error: 'Nick has to be 3-15 characters long.' };
+    } else if (/[0-9]/.test(nick.charAt(0))) {
+        return { valid: false, error: 'Nick can\'t start with digit' };
+    } else if (!(/^[A-Z\`a-z0-9[\]\\_\^{|}]+$/.test(nick))) {
+        let valid = [ 'a-z', '0-9', '[', ']', '\\', '`', '_', '^', '{', '|', '}' ];
+        return { valid: false, error: 'Illegal characters, allowed are ' + valid.join(', ') };
+    }
+
+    return { valid: true };
+}
