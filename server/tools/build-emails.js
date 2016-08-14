8d26ae26d3fa92d65e8692b3174b19e1fc3bd4b4
Ilkka Oksanen <iao@iki.fi>
2016-08-14 10:27:26 +0200

    Remove gulpfile, add replacement script

---
+++ server/tools/build-emails.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+const juice = require('juice');
+const mkdirp = require('mkdirp');
+
+const srcDir = path.join(__dirname, '..', 'emails');
+const dstDir = path.join(srcDir, 'build');
+const files = fs.readdirSync(srcDir);
+const isHBSfile = /\.hbs$/;
+
+mkdirp.sync(dstDir);
+
+files.filter(fileName => isHBSfile.test(fileName)).forEach(fileName => {
+    juice.juiceFile(path.join(srcDir, fileName), {}, (err, html) => {
+        if (err) {
+            throw err;
+        };
+
+        fs.writeFileSync(path.join(dstDir, fileName), html);
+    });
+});
