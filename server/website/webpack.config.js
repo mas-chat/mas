e62adc3fe601bbbcdb33a31e00323a6ca64a8884
Ilkka Oksanen <iao@iki.fi>
2016-08-21 11:22:19 +0200

    Install also bower

---
+++ server/website/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+const prefix = path.resolve(__dirname);
+
+module.exports = {
+    entry: `${prefix}/javascripts/app.js`,
+    output: {
+        path: `${prefix}/dist`,
+        publicPath: '/dist/',
+        filename: 'app.js'
+    },
+    module: {
+        loaders: [ {
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel',
+            query: {
+                presets: [ 'es2015' ]
+            }
+        }, {
+            test: /\.scss$/,
+            loaders: [ 'style', 'css', 'sass' ]
+        }, {
+            test: /\.css$/,
+            loader: 'style!css?modules'
+        }, {
+            test: /\.(ttf|eot|svg|woff|woff2)$/,
+            loader: 'url?limit=10000'
+        } ]
+    }
+};
