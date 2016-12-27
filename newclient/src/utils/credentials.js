33351da08b0f411abddf28e222e1bd4d74189e3b
Ilkka Oksanen <iao@iki.fi>
2016-12-27 16:26:17 +0100

    Fix export syntax

--- newclient/src/utils/credentials.js
+++ newclient/src/utils/credentials.js
@@ -1,5 +1,6 @@
 import Cookies from 'js-cookie';
 
 const authCookie = Cookies.get('auth') || '';
+const [ userId, secret ] = authCookie.split('-');
 
-export const [ userId, secret ] = authCookie.split('-');
+export { userId, secret };
