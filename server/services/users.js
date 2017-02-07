43888d946af78ec4dd3ffaf6ee5ac51383f0d07e
Ilkka Oksanen <iao@iki.fi>
2017-02-07 20:08:52 +0100

    Return user object even if creation fails

--- server/services/users.js
+++ server/services/users.js
@@ -26,19 +26,18 @@ const log = require('../lib/log');
 exports.addUser = async function addUser(details, { skipSetters = false } = {}) {
     const user = await User.create(details, { skipSetters });
 
-    if (!user.valid) {
+    if (user.valid) {
+        await Settings.create({ userId: user.id });
+
+        await NetworkInfo.create({
+            userId: user.id,
+            network: 'mas',
+            state: 'connected',
+            nick: user.get('nick')
+        });
+    } else {
         log.warn(`User creation failed, errors: ${JSON.stringify(user.errors)}`);
-        return null;
     }
-
-    await Settings.create({ userId: user.id });
-
-    await NetworkInfo.create({
-        userId: user.id,
-        network: 'mas',
-        state: 'connected',
-        nick: user.get('nick')
-    });
 
     return user;
 };
