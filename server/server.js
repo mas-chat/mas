826376f9651cf62ad63b9df9b02cf93f2dc020a4
Ilkka Oksanen <iao@iki.fi>
2017-02-24 08:34:48 +0100

    Simplify top level frontend server logic

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
-#!/usr/bin/env node
 //
-//   Copyright 2009-2014 Ilkka Oksanen <iao@iki.fi>
+//   Copyright 2009-2015 Ilkka Oksanen <iao@iki.fi>
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
@@ -24,46 +23,26 @@ init.configureProcess('frontend');
 const fs = require('fs');
 const http = require('http');
 const https = require('https');
-const conf = require('./lib/conf');
+const path = require('path');
+const koa = require('koa');
+const hbs = require('koa-hbs');
+const error = require('koa-error');
+const compress = require('koa-compress');
+const logger = require('koa-logger');
+const handlebarsHelpers = require('./lib/handlebarsHelpers');
 const log = require('./lib/log');
-const main = require('./main');
-
-const httpPort = conf.get('frontend:http_port');
-const httpsPort = conf.get('frontend:https_port');
+const redis = require('./lib/redis');
+const passport = require('./lib/passport');
+const authSessionChecker = require('./lib/authSessionChecker');
+const routes = require('./routes/routes');
+const scheduler = require('./lib/scheduler');
+const socketController = require('./controllers/socket');
+const conf = require('./lib/conf');
 
 let httpHandler = initialHandler;
 let httpsHandler = initialHandler;
 
-const httpServer = http.Server(httpHandlerSelector); // eslint-disable-line new-cap
-let httpsServer = null;
-
-httpServer.listen(httpPort, httpListenDone);
-log.info(`Frontend HTTP server listening: http://0.0.0.0:${httpPort}/`);
-
-if (conf.get('frontend:https')) {
-    const caCerts = [];
-    const caCertFileList = conf.get('frontend:https_ca');
-
-    if (caCertFileList) {
-        for (const file of caCertFileList.split(',')) {
-            caCerts.push(fs.readFileSync(file));
-        }
-    }
-
-    httpsServer = https.createServer({
-        key: fs.readFileSync(conf.get('frontend:https_key')),
-        cert: fs.readFileSync(conf.get('frontend:https_cert')),
-        ca: caCerts
-    }, httpsHandlerSelector);
-
-    httpsServer.listen(httpsPort, listensDone);
-    log.info(`MAS frontend https server listening, https://localhost:${httpsPort}/`);
-}
-
-function setHTTPHandlers(newHttpHandler, newHttpsHandler) {
-    httpHandler = newHttpHandler;
-    httpsHandler = newHttpsHandler;
-}
+createHTTPServers();
 
 function httpHandlerSelector(request, response) {
     httpHandler(request, response);
@@ -75,15 +54,107 @@ function httpsHandlerSelector(request, response) {
 
 function initialHandler(request, response) {
     response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.end('Server starting. Try again in few seconds...\n');
+    response.end('Server starting. Try again in a second...\n');
 }
 
-function httpListenDone() {
-    if (!conf.get('frontend:https')) {
-        listensDone();
+function createHTTPServers() {
+    const httpPort = conf.get('frontend:http_port');
+    const httpsPort = conf.get('frontend:https_port');
+    const httpServer = http.Server(httpHandlerSelector); // eslint-disable-line new-cap
+
+    httpServer.listen(httpPort, () => {
+        log.info(`Frontend HTTP server listening: http://0.0.0.0:${httpPort}/`);
+    });
+
+    if (conf.get('frontend:https')) {
+        const caCerts = [];
+        const caCertFileList = conf.get('frontend:https_ca');
+
+        if (caCertFileList) {
+            for (const file of caCertFileList.split(',')) {
+                caCerts.push(fs.readFileSync(file));
+            }
+        }
+
+        const httpsServer = https.createServer({
+            key: fs.readFileSync(conf.get('frontend:https_key')),
+            cert: fs.readFileSync(conf.get('frontend:https_cert')),
+            ca: caCerts
+        }, httpsHandlerSelector);
+
+        initApp(httpServer, httpsServer);
+
+        httpsServer.listen(httpsPort, () => {
+            log.info(`MAS frontend https server listening, https://localhost:${httpsPort}/`);
+        });
+    } else {
+        initApp(httpServer, null);
     }
 }
 
-function listensDone() {
-    main.init(httpServer, httpsServer, setHTTPHandlers);
+async function initApp(httpServer, httpsServer) {
+    const app = koa();
+
+    if (process.env.NODE_ENV === 'development') {
+        app.use(logger());
+    }
+
+    app.use(error());
+
+    app.use(function *xFrameOptionsMiddleware(next) {
+        this.set('X-Frame-Options', 'DENY');
+        yield next;
+    });
+
+    app.use(compress()); // Enable GZIP compression
+
+    app.use(passport.initialize());
+
+    app.use(hbs.middleware({
+        layoutsPath: path.join(__dirname, 'views/layouts'),
+        viewPath: path.join(__dirname, 'views'),
+        defaultLayout: 'main'
+    }));
+
+    app.use(authSessionChecker.processCookie);
+
+    handlebarsHelpers.registerHelpers(hbs);
+
+    log.info('Registering website routes');
+    routes.register(app);
+
+    scheduler.init();
+
+    // Socket.io server (socketController) must be created after last app.use()
+
+    if (conf.get('frontend:https')) {
+        socketController.setup(httpsServer);
+
+        const forceSSLApp = koa();
+
+        // To keep things simple, force SSL is always activated if https is enabled
+        forceSSLApp.use(function *forceSSLAppMiddleware() { // eslint-disable-line require-yield
+            this.response.status = 301;
+            this.response.redirect(conf.getComputed('site_url') + this.request.url);
+        });
+
+        httpHandler = forceSSLApp.callback();
+        httpsHandler = app.callback();
+    } else {
+        socketController.setup(httpServer);
+
+        httpHandler = app.callback();
+        httpsHandler = null;
+    }
+
+    init.on('beforeShutdown', async () => {
+        await socketController.shutdown();
+        scheduler.quit();
+    });
+
+    init.on('afterShutdown', async () => {
+        redis.shutdown();
+        httpServer.close();
+        log.quit();
+    });
 }
