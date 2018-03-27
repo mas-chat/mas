c1208b7c7c4aa4db8a2b65d6cd143cafc9eec8ca
Ilkka Oksanen <iao@iki.fi>
2018-03-27 22:27:21 +0200

    Remove unnecessary option

--- client/app/app.js
+++ client/app/app.js
@@ -19,8 +19,6 @@ import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
-Ember.MODEL_FACTORY_INJECTIONS = true;
-
 const App = Application.extend({
     modulePrefix: config.modulePrefix,
     podModulePrefix: config.podModulePrefix,
