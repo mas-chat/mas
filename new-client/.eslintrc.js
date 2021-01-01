929634929164ecda5e40e4c81adea7d2c8994f29
Ilkka Oksanen <iao@iki.fi>
2021-01-01 19:12:37 +0200

    Unify eslint config

--- new-client/.eslintrc.js
+++ new-client/.eslintrc.js
@@ -1,14 +1,22 @@
 module.exports = {
-  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  root: true,
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   parserOptions: {
     ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
-    sourceType: "module" // Allows for the use of imports
+    sourceType: 'module' // Allows for the use of imports
   },
   extends: [
-    "plugin:@typescript-eslint/recommended" // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint'
   ],
   rules: {
-    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+  },
+  settings: {
+    'import/extensions': ['.js', '.jsx'],
+    react: {
+      version: 'detect'
+    }
   }
 };
