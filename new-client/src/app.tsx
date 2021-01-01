93d8de6f789d1dbed14ffdca54bde29f6a71f681
Ilkka Oksanen <iao@iki.fi>
2021-01-01 19:14:33 +0200

    Remove ResponsiveContext

--- new-client/src/app.tsx
+++ new-client/src/app.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Box, Button, Grommet, ResponsiveContext, Sidebar, Avatar, Nav } from 'grommet';
 import { Help, Projects, Clock } from 'grommet-icons';
 
@@ -17,29 +17,25 @@ const theme = {
 
 const App = () => (
   <Grommet theme={theme} full>
-    <ResponsiveContext.Consumer>
-      {size => (
-        <Box fill direction="row">
-          <Box>
-            <Sidebar
-              background="brand"
-              header={<Avatar src="//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80" />}
-              footer={<Button icon={<Help />} hoverIndicator />}
-            >
-              <Nav gap="small">
-                <Button icon={<Projects />} hoverIndicator />
-                <Button icon={<Clock />} hoverIndicator />
-              </Nav>
-            </Sidebar>
-          </Box>
-          <Box flex overflow="hidden">
-            <Box flex align="center" justify="center">
-              app body
-            </Box>
-          </Box>
+    <Box fill direction="row">
+      <Box>
+        <Sidebar
+          background="brand"
+          header={<Avatar src="//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80" />}
+          footer={<Button icon={<Help />} hoverIndicator />}
+        >
+          <Nav gap="small">
+            <Button icon={<Projects />} hoverIndicator />
+            <Button icon={<Clock />} hoverIndicator />
+          </Nav>
+        </Sidebar>
+      </Box>
+      <Box flex overflow="hidden">
+        <Box flex align="center" justify="center">
+          app body
         </Box>
-      )}
-    </ResponsiveContext.Consumer>
+      </Box>
+    </Box>
   </Grommet>
 );
 
