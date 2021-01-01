9fefc22f0053510716d69eee116ae2fac38f3471
Ilkka Oksanen <iao@iki.fi>
2021-01-01 18:28:47 +0200

    Remove boilerplate from the app component

--- new-client/src/app.tsx
+++ new-client/src/app.tsx
@@ -1,110 +1,46 @@
 import React, { Component } from 'react';
-import {
-  Box,
-  Button,
-  Collapsible,
-  Heading,
-  Grommet,
-  Layer,
-  ResponsiveContext,
-} from 'grommet';
-import { FormClose, Notification } from 'grommet-icons';
+import { Box, Button, Grommet, ResponsiveContext, Sidebar, Avatar, Nav } from 'grommet';
+import { Help, Projects, Clock } from 'grommet-icons';
 
 const theme = {
   global: {
     colors: {
-      brand: '#228BE6',
+      brand: '#228BE6'
     },
     font: {
       family: 'Roboto',
       size: '14px',
-      height: '20px',
-    },
-  },
+      height: '20px'
+    }
+  }
 };
 
-const AppBar = (props) => (
-  <Box
-    tag='header'
-    direction='row'
-    align='center'
-    justify='between'
-    background='brand'
-    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
-    elevation='medium'
-    style={{ zIndex: '1' }}
-    {...props}
-  />
-);
-
-import './lib/socket';
-
-class App extends Component {
-  state = {
-    showSidebar: false,
-  }
-  render() {
-    const { showSidebar } = this.state;
-    return (
-      <Grommet theme={theme} full>
-        <ResponsiveContext.Consumer>
-          {size => (
-            <Box fill>
-              <AppBar>
-                <Heading level='3' margin='none'>My App</Heading>
-                <Button
-                  icon={<Notification />}
-                  onClick={() => this.setState({ showSidebar: !this.state.showSidebar })}
-                />
-              </AppBar>
-              <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
-                <Box flex align='center' justify='center'>
-                  app body
-                </Box>
-                {(!showSidebar || size !== 'small') ? (
-                  <Collapsible direction="horizontal" open={showSidebar}>
-                    <Box
-                      flex
-                      width='medium'
-                      background='light-2'
-                      elevation='small'
-                      align='center'
-                      justify='center'
-                    >
-                      sidebar
-                    </Box>
-                  </Collapsible>
-                ) : (
-                    <Layer>
-                      <Box
-                        background='light-2'
-                        tag='header'
-                        justify='end'
-                        align='center'
-                        direction='row'
-                      >
-                        <Button
-                          icon={<FormClose />}
-                          onClick={() => this.setState({ showSidebar: false })}
-                        />
-                      </Box>
-                      <Box
-                        fill
-                        background='light-2'
-                        align='center'
-                        justify='center'
-                      >
-                        sidebar
-                    </Box>
-                    </Layer>
-                  )}
-              </Box>
+const App = () => (
+  <Grommet theme={theme} full>
+    <ResponsiveContext.Consumer>
+      {size => (
+        <Box fill direction="row">
+          <Box>
+            <Sidebar
+              background="brand"
+              header={<Avatar src="//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80" />}
+              footer={<Button icon={<Help />} hoverIndicator />}
+            >
+              <Nav gap="small">
+                <Button icon={<Projects />} hoverIndicator />
+                <Button icon={<Clock />} hoverIndicator />
+              </Nav>
+            </Sidebar>
+          </Box>
+          <Box flex overflow={'hidden'}>
+            <Box flex align="center" justify="center">
+              app body
             </Box>
-          )}
-        </ResponsiveContext.Consumer>
-      </Grommet>
-    );
-  }
-}
+          </Box>
+        </Box>
+      )}
+    </ResponsiveContext.Consumer>
+  </Grommet>
+);
 
 export default App;
