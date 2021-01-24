b106bc92f1e36e6304e3b47c2fe77aafa53d4b12
Ilkka Oksanen <iao@iki.fi>
2021-01-24 12:52:28 +0200

    Add RootContainer

---
+++ new-client/src/components/RootContainer.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { observer } from 'mobx-react-lite';
+import { Flex, Box } from '@chakra-ui/react';
+import { Desktop, Sidebar } from '.';
+import type RootStore from '../stores/RootStore';
+
+interface RootContainerProps {
+  rootStore: RootStore;
+}
+
+const RootContainer: React.FunctionComponent<RootContainerProps> = ({ rootStore }: RootContainerProps) => {
+  return (
+    <Flex width="100vw" height="100vh" bgColor="white">
+      <Sidebar windowStore={rootStore.windowStore} />
+      <Desktop flex="1" rootStore={rootStore}></Desktop>
+    </Flex>
+  );
+};
+
+export default observer(RootContainer);
