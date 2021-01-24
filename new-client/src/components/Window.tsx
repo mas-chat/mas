721513db3b84712ce324c4e0848524e21b42edde
Ilkka Oksanen <iao@iki.fi>
2021-01-24 17:28:50 +0200

    Start to use virtuoso

--- new-client/src/components/Window.tsx
+++ new-client/src/components/Window.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { observer } from 'mobx-react-lite';
-import { Heading, Box } from '@chakra-ui/react';
+import { Box, Heading, Flex, Input, Text } from '@chakra-ui/react';
+import { Virtuoso } from 'react-virtuoso';
 import WindowModel from '../models/Window';
 
 interface WindowProps {
@@ -8,19 +9,44 @@ interface WindowProps {
 }
 
 const Window: React.FunctionComponent<WindowProps> = ({ window }: WindowProps) => {
+  const virtuoso = useRef(null);
+  const [atBottom, setAtBottom] = useState(false);
+  const messages = Array.from(window.messages.values());
+
+  const Row = ({ index }: { index: number }) => {
+    const message = messages[index];
+
+    return (
+      <Flex flexDirection="row">
+        <Box minWidth="50px">{message.decoratedTs}</Box>
+        <Box flex="1">
+          <Text as="b" flex="1">
+            {message.nick}:
+          </Text>{' '}
+          <Text as="span">{message.body}</Text>
+        </Box>
+      </Flex>
+    );
+  };
+
   return (
-    <Box bg="tomato" color="white" overflowY="scroll">
-      <Heading size="s" key={window.id}>
-        {window.simplifiedName}
-      </Heading>
-      {Array.from(window.messages.entries()).map(([messageKey, message]) => {
-        return (
-          <div key={messageKey}>
-            {message.nick}: {message.body}
-          </div>
-        );
-      })}
-    </Box>
+    <Flex key={window.id} flexDirection="column" bg="white" color="black">
+      <Heading size="s">{window.simplifiedName}</Heading>
+      <Box flex="1">
+        <Virtuoso
+          ref={virtuoso}
+          initialTopMostItemIndex={messages.length - 1}
+          style={{ height: '100%' }}
+          atBottomStateChange={bottom => {
+            setAtBottom(bottom);
+          }}
+          totalCount={messages.length}
+          itemContent={index => <Row index={index} />}
+          followOutput="smooth"
+        />
+      </Box>
+      <Input placeholder="small size" size="sm" />
+    </Flex>
   );
 };
 
