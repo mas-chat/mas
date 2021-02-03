af141663c7fd614fa39fe08d4be2a757f38856e7
Ilkka Oksanen <iao@iki.fi>
2021-02-03 17:41:03 +0200

    Remove unneeded config

--- new-client/webpack.common.js
+++ new-client/webpack.common.js
@@ -15,14 +15,6 @@ module.exports = {
         enforce: 'pre'
       },
       {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader?modules', 'postcss-loader', 'sass-loader']
-      },
-      {
         test: /\.(ttf|eot|svg|woff|woff2)$/,
         use: 'url-loader?limit=10000'
       },
