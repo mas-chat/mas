40ce37f45ae1f46e665bf821a314a3f8b4d6f3b8
Ilkka Oksanen <iao@iki.fi>
2021-02-07 12:01:22 +0200

    Initial dark mode support

--- new-client/src/index.tsx
+++ new-client/src/index.tsx
@@ -4,15 +4,24 @@ import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { ServerContextProvider } from './components/ServerContext';
 import { RootContainer } from './components';
 
-const config = {
-  initialColorMode: 'dark',
-  useSystemColorMode: false
-};
+// const config = {
+//   initialColorMode: 'light' as ColorMode,
+//   useSystemColorMode: false
+// };
 
-const theme = extendTheme(config);
+// const colors = {
+//   themeBg: '#252b2e',
+//   themeActiveBg: '#18242b',
+//   themeBrand: '#84c9fb',
+//   themeText: '#8f8f8f'
+// };
+
+const theme = extendTheme({
+  /* config, colors */
+});
 
 ReactDOM.render(
-  <ChakraProvider theme={theme}>
+  <ChakraProvider>
     <ServerContextProvider>
       <RootContainer />
     </ServerContextProvider>
