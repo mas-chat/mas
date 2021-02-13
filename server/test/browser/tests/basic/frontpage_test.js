695cf41ab16041dc845f78d475b5c36209e77cc1
Ilkka Oksanen <iao@iki.fi>
2021-02-13 14:47:51 +0200

    Remove obsolete browser tests

--- server/test/browser/tests/basic/frontpage_test.js
+++
@@ -1,9 +0,0 @@
-const utils = require('../../utils');
-
-module.exports = {
-  'Load front page': browser => {
-    browser.url('http://localhost:2222').waitForElementVisible('body').assert.containsText('button', 'Sign In').end();
-  },
-
-  tearDown: utils.tearDown
-};
