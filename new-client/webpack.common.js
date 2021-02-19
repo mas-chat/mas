861ad6e7ddc92da50303557deab9a89ac5689d7f
Ilkka Oksanen <iao@iki.fi>
2021-02-19 19:13:46 +0200

    Remove unnecessary loaders

--- new-client/webpack.common.js
+++ new-client/webpack.common.js
@@ -13,14 +13,6 @@ module.exports = {
         test: /\.js$/,
         use: 'source-map-loader',
         enforce: 'pre'
-      },
-      {
-        test: /\.(ttf|eot|svg|woff|woff2)$/,
-        use: 'url-loader?limit=10000'
-      },
-      {
-        test: /\.(png|jpg)$/,
-        use: 'url-loader?limit=8192'
       }
     ]
   }
