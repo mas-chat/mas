4de2bc9e64863b94e82dc7c4b88c1348d763acc6
Ilkka Oksanen <iao@iki.fi>
2021-02-24 00:14:04 +0200

    Fix currentDate hook

--- new-client/src/hooks/currentDate.ts
+++ new-client/src/hooks/currentDate.ts
@@ -1,16 +1,21 @@
 import { useCallback, useEffect, useState } from 'react';
 import dayjs, { Dayjs } from 'dayjs';
 
-const millisecondsToMidnight = () => dayjs().add(1, 'day').startOf('date').diff(dayjs());
+// Plus one to make sure rounding doesn't interfere
+const msToMidnight = () => dayjs().add(1, 'day').startOf('date').diff(dayjs()) + 1;
 
 export function useCurrentDate(): Dayjs {
   const [currentDate, setCurrentDate] = useState(dayjs());
+
   const updateDate = useCallback(() => {
-    setCurrentDate(dayjs());
-    setTimeout(updateDate, millisecondsToMidnight());
+    const currentDate = dayjs();
+    const msToNextDay = msToMidnight();
+
+    setCurrentDate(currentDate);
+    setTimeout(updateDate, msToNextDay);
   }, []);
 
-  useEffect(() => updateDate, [updateDate]);
+  useEffect(updateDate, [updateDate]);
 
   return currentDate;
 }
