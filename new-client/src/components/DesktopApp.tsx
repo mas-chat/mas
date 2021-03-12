aa077d0bbe2f681c8da12bb0a526a7f8dcdfbbf8
Ilkka Oksanen <iao@iki.fi>
2021-03-12 20:35:39 +0200

    Improve default route routing

--- new-client/src/components/DesktopApp.tsx
+++ new-client/src/components/DesktopApp.tsx
@@ -4,7 +4,7 @@ import { Flex } from '@chakra-ui/react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { Desktop, Sidebar, WindowSettings, Welcome } from '.';
 import { ServerContext } from './ServerContext';
-import { windowUrl } from '../lib/urls';
+import { windowUrl, welcomeUrl } from '../lib/urls';
 
 interface DesktopAppProps {
   firstRenderComplete: () => void;
@@ -15,6 +15,16 @@ const DesktopApp: FunctionComponent<DesktopAppProps> = ({ firstRenderComplete }:
 
   useEffect(() => firstRenderComplete(), [firstRenderComplete]);
 
+  const defaultRedirectUrl = () => {
+    if (windowStore.windows.size > 0) {
+      const window = windowStore.activeWindow || windowStore.nextActiveWindow || windowStore.firstWindow;
+
+      return windowUrl({ windowId: window.id as number });
+    } else {
+      return welcomeUrl();
+    }
+  };
+
   return (
     <Flex width="100vw" height="100vh">
       <Sidebar showDesktops={true} />
@@ -22,10 +32,7 @@ const DesktopApp: FunctionComponent<DesktopAppProps> = ({ firstRenderComplete }:
         <Route path="welcome" element={<Welcome />} />
         <Route path="c/:windowId" element={<Desktop />} />
         <Route path="c/:windowId/settings" element={<WindowSettings />} />
-        <Route
-          path="*"
-          element={<Navigate to={windowUrl({ windowId: windowStore.startupActiveWindow?.id as number })} />}
-        />
+        <Route path="*" element={<Navigate to={defaultRedirectUrl()} />} />
       </Routes>
     </Flex>
   );
