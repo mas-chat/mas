4b957101c2984c92276dcf98592f140962864a75
Ilkka Oksanen <iao@iki.fi>
2021-03-12 20:33:25 +0200

    Add welcome url helper and route

--- new-client/src/components/DesktopApp.tsx
+++ new-client/src/components/DesktopApp.tsx
@@ -2,7 +2,7 @@ import React, { FunctionComponent, useContext, useEffect } from 'react';
 import { observer } from 'mobx-react-lite';
 import { Flex } from '@chakra-ui/react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { Desktop, Sidebar, WindowSettings } from '.';
+import { Desktop, Sidebar, WindowSettings, Welcome } from '.';
 import { ServerContext } from './ServerContext';
 import { windowUrl } from '../lib/urls';
 
@@ -19,6 +19,7 @@ const DesktopApp: FunctionComponent<DesktopAppProps> = ({ firstRenderComplete }:
     <Flex width="100vw" height="100vh">
       <Sidebar showDesktops={true} />
       <Routes basename="/app">
+        <Route path="welcome" element={<Welcome />} />
         <Route path="c/:windowId" element={<Desktop />} />
         <Route path="c/:windowId/settings" element={<WindowSettings />} />
         <Route
--- new-client/src/lib/urls.ts
+++ new-client/src/lib/urls.ts
@@ -4,6 +4,8 @@ const encodeId = (id: number): string => (id + BASE_ID).toString(36).toUpperCase
 
 export const rootUrl = (): string => '/app';
 
+export const welcomeUrl = (): string => '/app/welcome';
+
 export const windowUrl = ({ windowId }: { windowId: number }): string => `/app/c/${encodeId(windowId)}`;
 
 export const windowSettingsUrl = ({ windowId }: { windowId: number }): string =>
