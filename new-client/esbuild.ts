26c06950e73f4e425d73537e656bb2a5a430d797
Ilkka Oksanen <iao@iki.fi>
2021-12-31 12:14:37 +0200

    Bring back single quotes

--- new-client/esbuild.ts
+++ new-client/esbuild.ts
@@ -1,15 +1,15 @@
-import { writeFileSync, emptyDirSync } from "fs-extra";
-import { build, BuildOptions } from "esbuild";
-import yargs from "yargs/yargs";
-import { hideBin } from "yargs/helpers";
+import { writeFileSync, emptyDirSync } from 'fs-extra';
+import { build, BuildOptions } from 'esbuild';
+import yargs from 'yargs/yargs';
+import { hideBin } from 'yargs/helpers';
 
 const argv = yargs(hideBin(process.argv))
   .options({
-    devMode: { type: "boolean", default: false },
+    devMode: { type: 'boolean', default: false }
   })
   .parseSync();
 
-const outDir = "dist";
+const outDir = 'dist';
 
 async function buildAll(devMode = false) {
   console.log(`DEV mode: ${devMode}`);
@@ -17,22 +17,22 @@ async function buildAll(devMode = false) {
   emptyDirSync(outDir);
 
   const buildConf: BuildOptions = {
-    logLevel: "info",
-    entryPoints: ["src/index.tsx"],
-    entryNames: devMode ? "[name]" : "[name]-[hash]",
+    logLevel: 'info',
+    entryPoints: ['src/index.tsx'],
+    entryNames: devMode ? '[name]' : '[name]-[hash]',
     minify: !devMode,
     bundle: true,
     outdir: outDir,
-    loader: { ".png": "file" },
+    loader: { '.png': 'file' },
     metafile: !devMode,
-    watch: devMode,
+    watch: devMode
   };
 
   if (devMode) {
     build(buildConf);
   } else {
     const result = await build(buildConf);
-    writeFileSync("dist/meta.json", JSON.stringify(result.metafile));
+    writeFileSync('dist/meta.json', JSON.stringify(result.metafile));
   }
 }
 
