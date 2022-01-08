0317c28681ce699ce11abd0a2ede9893497a7777
Ilkka Oksanen <iao@iki.fi>
2022-01-08 16:14:31 +0200

    Add initial emoji picker

---
+++ new-client/src/components/WindowEmojiPicker.tsx
@@ -0,0 +1,42 @@
+import React, { FunctionComponent, useState } from 'react';
+import { Picker, EmojiData } from 'emoji-mart';
+import { IconButton, Button, Popover, PopoverArrow, PopoverContent, PopoverTrigger } from '@chakra-ui/react';
+import { IoHappyOutline } from 'react-icons/io5';
+import { MessageRemirrorManager } from '../hooks/remirror';
+
+interface WindowMenuProps {
+  manager: MessageRemirrorManager;
+}
+
+const WindowMenu: FunctionComponent<WindowMenuProps> = ({ manager }: WindowMenuProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const open = () => setIsOpen(!isOpen);
+  const close = () => setIsOpen(false);
+
+  const handleSelect = (emoji: EmojiData) => {
+    console.log(emoji);
+    manager.store.commands.addEmoji(emoji.native);
+    close();
+  };
+
+  return (
+    <Popover isLazy isOpen={isOpen} onClose={close}>
+      <PopoverTrigger>
+        <Button
+          as={IconButton}
+          onClick={open}
+          aria-label="Options"
+          icon={<IoHappyOutline />}
+          variant="outline"
+          size="sm"
+        />
+      </PopoverTrigger>
+      <PopoverContent width="auto">
+        <PopoverArrow />
+        <Picker set="apple" theme="dark" native={true} title="Select emoji" onSelect={handleSelect} />
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+export default WindowMenu;
